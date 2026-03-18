ab9de2809e7dd91dd49d87cdf6f19532288e7ce1
shara <your-email@example.com>
2026-03-18 14:54:54 +0530

    fgghdsfg

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/postcss'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/shweta-portfolio/',
   plugins: [react()],
   css: {
     postcss: {
